d336688451f825c71a0272339b92b854c75cabf7
Raúl Fernández Tirado <raulfdeztdo@gmail.com>
2025-08-27 21:42:36 +0200

    refactor(build): simplify build script by removing empty lines
    Remove unnecessary empty lines in build.sh to improve readability and maintainability

--- build.sh
+++ build.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-
 # Instala las dependencias
 npm install
-
 # Ejecuta el comando grab para movistarplus.es
 npm run grab --- --channels=sites/movistarplus.es/movistarplus.es.channels.xml --maxConnections=5
