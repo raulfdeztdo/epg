cc850eb7d9e8a04e36393d92ec181eb405d6fa43
Raúl Fernández Tirado <raulfdeztdo@gmail.com>
2025-08-29 01:30:01 +0200

    fix(guide): change timezone from UTC to Europe/Madrid in guide generation
    refactor(movistarplus): add timezone support and improve error handling

--- scripts/models/guide.ts
+++ scripts/models/guide.ts
@@ -2,34 +2,34 @@ import { Collection, DateTime } from '@freearhey/core'
 import { generateXMLTV } from 'epg-grabber'
 
 interface GuideData {
-  channels: Collection
-  programs: Collection
-  filepath: string
-  gzip: boolean
+    channels: Collection
+    programs: Collection
+    filepath: string
+    gzip: boolean
 }
 
 export class Guide {
-  channels: Collection
-  programs: Collection
-  filepath: string
-  gzip: boolean
+    channels: Collection
+    programs: Collection
+    filepath: string
+    gzip: boolean
 
-  constructor({ channels, programs, filepath, gzip }: GuideData) {
-    this.channels = channels
-    this.programs = programs
-    this.filepath = filepath
-    this.gzip = gzip || false
-  }
+    constructor({ channels, programs, filepath, gzip }: GuideData) {
+        this.channels = channels
+        this.programs = programs
+        this.filepath = filepath
+        this.gzip = gzip || false
+    }
 
-  toString() {
-    const currDate = new DateTime(process.env.CURR_DATE || new Date().toISOString(), {
-      timezone: 'UTC'
-    })
+    toString() {
+        const currDate = new DateTime(process.env.CURR_DATE || new Date().toISOString(), {
+            timezone: 'Europe/Madrid'
+        })
 
-    return generateXMLTV({
-      channels: this.channels.all(),
-      programs: this.programs.all(),
-      date: currDate.toJSON()
-    })
-  }
+        return generateXMLTV({
+            channels: this.channels.all(),
+            programs: this.programs.all(),
+            date: currDate.toJSON()
+        })
+    }
 }
